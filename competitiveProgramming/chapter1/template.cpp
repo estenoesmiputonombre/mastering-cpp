14d9cfad315186908031f3e0d906e4c08ab53662
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2021-02-14 18:36:15 +0100

    Adding template to competitive programming

---
+++ competitiveProgramming/chapter1/template.cpp
@@ -0,0 +1,16 @@
+/**
+ * #include <bits/stdc++.h>
+ * // This is a bad practise, because the libraries that are included in the bits folder, area compiler specific.
+ * This library contains all the typical libraries that have to use in the context of competitive programming.
+ */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// We can compile this program using the following command:
+// g++ -std=c++11 -O2 -Wall template.cpp -o template
+int main() {
+
+    return 0;
+}
